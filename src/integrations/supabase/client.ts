1f9b0109b497b4a3dc9d34d38754101ca6370f5d
doug-aut8 <codclick.aut8@gmail.com>
2026-03-14 15:08:06 -0300

    Update projeto supabase client.ts

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -2,8 +2,8 @@
 import { createClient } from '@supabase/supabase-js';
 import type { Database } from './types';
 
-const SUPABASE_URL = "https://aealgiyzbenbhhftwkxb.supabase.co";
-const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_segSu4lwfe4romisMtKB2g_TqiO70r8";
+const SUPABASE_URL = "https://bmlqjunwvgtrubqtplhk.supabase.co";
+const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_cJjsUvcjGWuguWyOrdetwA_62vR_ud4";
 
 // Import the supabase client like this:
 // import { supabase } from "@/integrations/supabase/client";
